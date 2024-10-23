deaf2013e8883b092134784967cc06769b5e94fe
Zulusar <150274779+Zulusar@users.noreply.github.com>
2024-10-23 19:17:41 +0500

    qwerty

--- dist/board.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function boardFromString(s: string): string[];
-export declare function boardToString(b: string[]): string;
-export declare function isFill(board: string[]): boolean;
-export declare function isRightMove(move: number, board: string[]): boolean;
-export declare function checkWin(board: string[]): string;
